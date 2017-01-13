e477ea282b5f0c210c9ab983b73ec2c44fa05f1d
Sean Duffy <seanduffy@live.com>
2017-01-13 02:45:38 +0000

    Fix help formatting

--- bot/messenger.py
+++ bot/messenger.py
@@ -21,7 +21,7 @@ class Messenger(object):
 
     def write_help_message(self, channel_id):
         bot_uid = self.clients.bot_user_id()
-        txt = '{}\n{}\n{}\n{}'.format(
+        txt = '{}\n{}\n{}'.format(
             "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
             "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
             "> `<@" + bot_uid + "> freq [band] [channel]` - I'll tell you the frequency of an FPV channel.")
